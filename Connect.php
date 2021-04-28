49d57b9fc5ef0559c236eba852dc68d92544f735
root <luann4099@gmail.com>
2021-04-28 11:08:22 +0700

    create connect

---
+++ Connect.php
@@ -0,0 +1,42 @@
+<?php
+
+class Connect
+{
+    private $dbHost = 'localhost';
+    private $dbName = 'shop_cp17';
+    private $dbUser = 'root';
+    private $dbPass = 'koodinh';
+    protected $pdo = null;
+
+
+    //kết nối đến CSDL sử dụng PDO
+
+    /**
+     * Connect constructor.
+     */
+    public function __construct ()
+    {
+        $this->getConnect();
+    }
+
+    public function getConnect ()
+    {
+        try {
+            $this->pdo = new PDO("mysql:host=$this->dbHost;dbname=$this->dbName", $this->dbUser, $this->dbPass);
+        } catch (PDOException $e) {
+            echo 'Lỗi kết nối CSDL';
+        }
+    }
+
+    public function getPDO ()
+    {
+        return $this->pdo;
+    }
+
+    public function closeConnect ()
+    {
+        $this->pdo = null;
+    }
+}
+
+?>
